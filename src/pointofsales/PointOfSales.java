39405a43c04e1ed6ea74ce11e66e36923763da2e
krimint <jumpy.3gp@gmail.com>
2021-10-24 21:29:15 +0700

    Integrasi Login

--- src/pointofsales/PointOfSales.java
+++ src/pointofsales/PointOfSales.java
@@ -5,6 +5,10 @@
  */
 package pointofsales;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author HP
@@ -14,8 +18,28 @@ public class PointOfSales {
     /**
      * @param args the command line arguments
      */
+    
+    private static Connection MySQLConfig;
+    
+    public static Connection configDB()throws SQLException
+    {
+        try {
+            String url = "jdbc:mysql://localhost:3306/projectsdp";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url,user,pass);
+        } catch (SQLException e) {
+            System.out.println("Koneksi ke DB Gagal");
+        }
+        return MySQLConfig;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        FormLogin frm = new FormLogin(/*arr1*/);
+        frm.setVisible(true);
     }
     
 }
